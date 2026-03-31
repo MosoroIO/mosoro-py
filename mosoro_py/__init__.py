e2841f30fca8f269ef6e307f3cfca26ecda17813
mikestemple <mike@inspirer.com>
2026-03-31 09:09:52 -0700

    chore: wire models.py into __init__.py exports

--- mosoro_py/__init__.py
+++ mosoro_py/__init__.py
@@ -2,6 +2,25 @@
 
 from mosoro_py.client import MosoroClient
 from mosoro_py.mqtt import MosoroMQTT
+from mosoro_py.models import (
+    CurrentTask,
+    ErrorDetail,
+    MessageHeader,
+    MosoroMessage,
+    MosoroPayload,
+    Position,
+)
 
 __version__ = "0.1.0"
-__all__ = ["MosoroClient", "MosoroMQTT", "__version__"]
+__all__ = [
+    "MosoroClient",
+    "MosoroMQTT",
+    # Models (re-exported from mosoro-core or standalone)
+    "MosoroMessage",
+    "MosoroPayload",
+    "Position",
+    "MessageHeader",
+    "CurrentTask",
+    "ErrorDetail",
+    "__version__",
+]
